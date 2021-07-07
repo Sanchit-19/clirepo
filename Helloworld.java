60ab829e4ae8f37eecf6c78656e696760972ae56
Sanchit-19 <sanchit.jain@capgemini.com>
2021-07-07 22:57:54 +0530

    My main method

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,10 @@
 public class Helloworld{
 
 public static void main(String[] args){
-System.out.println("Hello From Java")
+System.out.println("Hello From Java");
+System.out.println("++++++++++++++++");
+System.out.println("Hello From Jav2");
+System.out.println("+++++++++++++");
+
 }
 }
